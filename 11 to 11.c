88014636062cc1cda32843bf7f44fe65814e36ac
tauqeerjawed <74541787+tauqeerjawed@users.noreply.github.com>
2020-11-16 17:03:59 +0500

    TABLE OF 11
    Table of 11 
    in C PRogram

---
+++ 11 to 11.c
@@ -0,0 +1,20 @@
+main()
+{
+	int t;
+	int j;
+	int x;
+	//for(t=1;t<=10;t++){
+	//int c=t*j;
+	//printf ("%d*%d=%d\n",t,j,c);}
+		
+	
+	 scanf("%d", &t);
+	 scanf("%d", &j);
+	 scanf("%d", &x);
+	 while(j<=x)
+	 {
+	 	printf("%d*%d=%d",t,j,\t*j);
+	 	j++;
+	 }
+}
+
